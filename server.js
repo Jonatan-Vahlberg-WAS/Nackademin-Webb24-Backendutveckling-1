d06a40c9b8291cc38c922622a3ae4740aedb7af1
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-02-28 15:03:41 +0100

    add code for POST req

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const fs = require("fs/promises")
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+// Tillåt inkommande JSON data
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("API")
@@ -19,6 +20,28 @@ async function getStudents(program) {
     return students
 }
 
+async function saveStudents(students) {
+    await fs.writeFile('data/students.json', JSON.stringify(students, null, 2))
+}
+
+async function createStudent(data) {
+    if(!data.firstName || !data.lastName || !data.program){
+        throw new Error("Invalid data")
+    }
+    const students = await getStudents()
+    const newStudent = {
+        id: Math.floor(Math.random() * 10000),
+        firstName: data.firstName,
+        lastName: data.lastName,
+        program: data.program
+    }
+
+    students.push(newStudent)
+    await saveStudents(students)
+    return newStudent
+    
+}
+
 app.get("/api/students/", async (req, res) => {
     const program = req.query.program
     console.log(program)
@@ -30,6 +53,19 @@ app.get("/api/students/", async (req, res) => {
         res.status(500).json({
             error: "Error fetching students"
         })   
+    }
+})
+
+app.post("/api/students/", async (req, res) => {
+    const body = req.body
+    try {
+        const student = await createStudent(body)
+        res.status(201).json(student)
+    } catch (error) {
+        console.warn("Error: creating student", error);
+        res.status(400).json({
+            error: error.message
+        })  
     }
 })
 
